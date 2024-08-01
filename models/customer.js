d1b233323f91a29960543d4953c71e60bdbed2ac
Diego Sotorivero <123613958+DSr2111@users.noreply.github.com>
2024-08-01 06:34:14 -0400

    using full name getter

--- models/customer.js
+++ models/customer.js
@@ -53,6 +53,12 @@ class Customer {
     return new Customer(customer);
   }
 
+  // get customer full name
+
+  get fullName() {
+    return `${this.firstName} ${this.lastName}`;
+  }
+
   /** get all reservations for this customer. */
 
   async getReservations() {
